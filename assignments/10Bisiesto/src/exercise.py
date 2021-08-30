91d87335f42e56ec87dee9da417f84e9b80296ca
A01661736 <88848934+A01661736@users.noreply.github.com>
2021-08-30 16:20:56 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,25 +1,18 @@
 def bisiesto(a):
-    
-    if (a/4==0):
-        a==True
-        
-
-    if (a/4==0 )and(a/100==0):
-        if (a/400==0):
-            a==True
-          
-        else:
-            a==False
-            
-    b=print(str(a))
-    return  b
+    bis=False
+    if (a%4==0):
+        bis=True
+    elif (a%100==0)and(a%400==0):
+        bis=True
+    return  bis
 
 def main():
     #escribe tu código abajo de esta línea
     print ("Dame el año")
     inpu=int(input())
 
-    bisiesto(inpu)
+##bisiesto(inpu)
+        
     L=bisiesto(inpu)
 
     print (str(L))
