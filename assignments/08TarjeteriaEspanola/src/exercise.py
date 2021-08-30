c31561b2095cee5311a4f736d953feef79b0fac1
A01661736 <88848934+A01661736@users.noreply.github.com>
2021-08-30 11:07:19 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,6 +1,22 @@
+def tarjetas(a,b):
+    a*=12
+    b*=35
+    if (a>b):
+        c=b
+    elif (b>a):
+        c=a
+    return c
 
 def main():
-    #escribe tu código abajo de esta línea
+    print ("Dame la cantidad de pliegos de papel albanene")
+    al=int(input())
+    print ("Dame la cantidad de pliegos de plumones")
+    pl=int(input())
+
+    tarjetas(al,pl)
+    T=tarjetas(al,pl)
+
+    print ("Se pueden hacer: " + str(T) + " tarjetas")
     pass
 
 if __name__=='__main__':
